274be5145e41b6ec81dcb49a7c3d577a203a0d8d
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-05-26 04:08:24 -0800

    Add files via upload

---
+++ lecturecodes/try.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class Try {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println(br.readLine());
+		
+	/*	float a = Float.parseFloat(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+
+		System.out.println(a);
+		System.out.println(b);
+	*/
+	}
+}
+
---
+++ lecturecodes/try1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class Try {
+
+	public static void main(String[] args) {
+
+
+	System.out.println("Hiii");
+
+	}
+
+//	System.out.println("Hiii");	// Class chya scope chya baher chalt nahiye,2nd class create krun tyat chalu shkt.
+}
