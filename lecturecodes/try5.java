499714999cfd5947dafd1f687eafc9060240e67d
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-05-27 02:35:20 -0800

    Add files via upload

---
+++ lecturecodes/try5.java
@@ -0,0 +1,13 @@
+
+class string {
+
+	public static void main(String[] args) {
+
+		String str = "Prathm";
+		System.out.println(str);
+
+		String str1 = new String("Shinde");
+		System.out.println(str1);
+
+	}
+}
