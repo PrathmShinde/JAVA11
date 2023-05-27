ce81eb202ae80109f9a43eddfdb4bc0cf1ab2616
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-05-26 18:41:17 -0800

    Add files via upload

---
+++ lecturecodes/try2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+class Try {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		System.out.println(input);
+
+		
+		BufferedReader bk = new BufferedReader(new InputStreamReader(System.in));
+		String in = bk.readLine();
+		System.out.println(in);
+	}
+
+
+}
