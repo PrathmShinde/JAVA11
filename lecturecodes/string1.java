3f0c97edae84d2c80480c50804b1d6f544826318
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-05-28 08:17:05 -0800

    Add files via upload

---
+++ lecturecodes/string1.java
@@ -0,0 +1,23 @@
+/*
+
+   Topic - String
+   command line argument input
+   lect 45
+
+*/
+class StringDemo {
+
+	public static void main(String[] args) {
+
+		for(int i = 0; i<args.length; i++) {
+
+			System.out.println(args[i]);
+		}
+	}
+}
+
+/*	
+  Input:
+  java <class name> Prathm Sagya Anya Saurya
+ 
+*/
