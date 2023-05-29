ca801e2c622e611cdff18afdd11fb334d7b64ee4
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-05-29 03:28:59 -0800

    Add files via upload

---
+++ lecturecodes/string5.java
@@ -0,0 +1,18 @@
+// String Constructor demo
+// lect - 48
+
+class StringConstrDemo {
+
+	public static void main(String[] args) {
+
+		String s1 = new String();
+		System.out.println(s1);			//null
+		System.out.println(s1.length());	//0
+		
+		String s2 = new String("Core2Web");
+		System.out.println(s2);			//Core2Web
+		System.out.println(s2.length());	//8
+	}
+}
+
+// length men=thod cha return type int aahe
