c8a02457a9e170a63e600ab03a12b95bec410b42
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-05-31 08:09:09 -0800

    Add files via upload

---
+++ lecturecodes/string15.java
@@ -0,0 +1,19 @@
+/*
+ 
+   lect 50
+   String class Methods
+   lastIndexOf()
+
+*/
+
+class IndexOf {
+
+        public static void main(String[] args) {
+
+		String s1 = new String("Bielearn");
+		String s2 = new String("BieMedia");
+		String s3 = new String("BieTickets");
+
+		System.out.println(s1.lastIndexOf('e'));	//4  --> magun suruvat krto check krayla
+	}
+}
