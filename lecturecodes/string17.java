5ff7f27be6b4c26709921bbd576e243578888c8f
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-02 01:14:36 -0800

    Add files via upload

---
+++ lecturecodes/string17.java
@@ -0,0 +1,24 @@
+/*
+ 
+   lect 50
+   String class Methods
+   toUpperCase();
+   toLowerCase();
+   
+
+*/
+
+class StringDemo {
+
+        public static void main(String[] args) {
+
+                String s1 = new String("Bielearn");
+                String s2 = new String("BieMedia");
+                String s3 = new String("BieTickets");
+
+
+		System.out.println(s1.toUpperCase());
+		System.out.println(s1.toLowerCase());
+	}
+}
+
