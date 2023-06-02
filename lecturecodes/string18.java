e2952b868181e25084229fc65c07faf83a507be6
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-02 01:14:54 -0800

    Add files via upload

---
+++ lecturecodes/string18.java
@@ -0,0 +1,15 @@
+/*
+ 
+   lect 50
+   StringBuffer class
+   
+   */
+
+class StringBufferDemo {
+
+        public static void main(String[] args) {
+
+		StringBuffer sb1 = new StringBuffer("Dentist");
+		StringBuffer sb2 = "Shashi";	//Error
+	}
+}
