cbdca7bdaa4dafbd68b78e40f7d381f73aa632d6
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-04 05:06:57 -0800

    Add files via upload

---
+++ lecturecodes/string24.java
@@ -0,0 +1,13 @@
+
+
+class ConcatDemo {
+
+	public static void main(String[] args) {
+
+		String s1 = "Shashi";
+		String s2 = "Bagal";
+
+		System.out.println(s1.concat(s2));	//See bytecode and observe changes
+	
+	}
+}
