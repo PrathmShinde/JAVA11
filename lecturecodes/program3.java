65a2d0a8c8780828c4b31dd17c3f0ee3eb685f1b
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-05 06:14:20 -0800

    Add files via upload

---
+++ lecturecodes/program3.java
@@ -0,0 +1,23 @@
+// create two classes in single file(Possible if both classes should not be public) 
+
+
+class C2W{
+
+  	public static void main(String[] args) {
+
+		System.out.println("In C2W");
+	}
+}
+
+class Biencaps {
+
+	public static void main(String[] args) {
+
+		System.out.println("In Biencaps");
+	}
+}
+
+// Result :
+// we can create more than one classes in a single java file(i.e. program1.java)
+// but we can access only one .class file
+
