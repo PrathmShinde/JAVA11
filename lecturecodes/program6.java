346e9b377d38484c453d1a7e988fa5c6fd16bea1
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-06 09:43:20 -0800

    Add files via upload

---
+++ lecturecodes/program6.java
@@ -0,0 +1,14 @@
+// java Wrapper class
+// java SIZEof operator ( in java, we cannot use sizeof Operator/keyword )
+// Instead of sizeoff we can use below codeLines.
+
+class SizeOf {
+
+	public static void main(String[] args) {
+
+		System.out.println(Integer.SIZE/8);	//4
+		System.out.println(Character.SIZE/8);	//2
+		System.out.println(Float.SIZE/8);	//4
+		System.out.println(Double.SIZE/8);	//8			
+	}
+}
