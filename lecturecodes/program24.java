30ae8ef693362d785ff2e616eb3d522dba6047f4
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-13 17:58:04 -0800

    Add files via upload

---
+++ lecturecodes/program24.java
@@ -0,0 +1,20 @@
+//Topic : Array
+//get a class Name by using getClass() method
+
+class IPL {
+
+	void matches() {
+
+		System.out.println("RCB vs MI");
+	}
+
+	public static void main(String[] args) {
+
+		IPL ipl = new IPL();
+		ipl.matches();
+
+		System.out.println(ipl.getClass());
+	}
+}
+
+//See the next code --> vim program25.java
