0c3518db0d7b30f9551130e18c3a20c075afed96
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-13 17:58:41 -0800

    Add files via upload

---
+++ lecturecodes/program26.java
@@ -0,0 +1,13 @@
+
+
+class ArrayDemo {
+
+	public static void main(String[] args) {
+
+		int[] shashi = new int[5];
+		float[] farr = new float[5];
+
+		System.out.println(shashi.getClass());	//class [I
+		System.out.println(farr.getClass());	//class [F
+	}
+}
