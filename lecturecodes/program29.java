e15b438b767842dde5c108f01862c4ba02b2993e
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-14 08:17:01 -0800

    Add files via upload

---
+++ lecturecodes/program29.java
@@ -0,0 +1,29 @@
+// Multi-Dimentional array
+// get input from user
+
+import java.util.*;
+class Multiarr {
+
+	public static void main(String[] args) {
+
+		int marr[][] = new int[3][3];
+
+		Scanner sc = new Scanner(System.in);
+
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+
+				marr[i][j] = sc.nextInt();
+			}
+		}
+
+		System.out.println("Array Elements are : ");
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+
+				System.out.print(marr[i][j]+ " ");
+			}
+			 	System.out.println();
+		}
+	}
+}
