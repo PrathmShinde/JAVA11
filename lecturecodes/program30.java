ca99bdfd959cdc751c60c27a7dd281ed0f862e9b
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-14 08:17:20 -0800

    Add files via upload

---
+++ lecturecodes/program30.java
@@ -0,0 +1,21 @@
+//Multi-Dimentional array
+//Hardcoded values
+//Do trial and error
+
+import java.util.*;
+class Multiarr {
+
+	public static void main(String[] args) {
+
+		int marr[][] = {{1,2,3},{1,2,3},{1,2,3}}; //jevdhe brackets astat tevdhe row astat ani tya brackets mdhe jevdhe numbers astat tevdhe columns astat
+
+		 for(int i=0;i<3;i++) {
+                        for(int j=0;j<3;j++) {
+
+                                System.out.print(marr[i][j]+ " ");
+                        }
+                                System.out.println();
+                }
+
+	}
+}
